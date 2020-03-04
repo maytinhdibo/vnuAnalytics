b52e76f9ffea8ed27bb29f0d7f6abd6bfa120b49
maytinhdibo <iammaytinhdibo@gmail.com>
2020-03-04 08:24:39 +0700

    signed

--- controllers/grid/AnalyticsGridHandler.inc.php
+++ controllers/grid/AnalyticsGridHandler.inc.php
@@ -5,6 +5,7 @@
  *
  * Copyright (c) 2014-2018 Simon Fraser University
  * Copyright (c) 2003-2018 John Willinsky
+ * Copyright (c) 2019-2020 Cuong Tran
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class StaticPageGridHandler
